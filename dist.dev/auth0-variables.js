5907d8b1f26e85a28acf4f6b3284a73ecab115bf
Mohammad Umar <umarsoft.java@gmail.com>
2018-03-25 22:34:01 +0530

    Update auth0-variables.js

--- dist.dev/auth0-variables.js
+++ dist.dev/auth0-variables.js
@@ -1,5 +1,5 @@
 var AUTH0_CLIENT_ID='cO02S01qm4vmAp2jhUtuLI4iAfQgXNx4'; 
 var AUTH0_DOMAIN='zones.auth0.com'; 
-var AUTH0_CALLBACK_URL='http://localhost:3000/callback';
+var AUTH0_CALLBACK_URL='http://67.205.167.209:3000/callback';
 //var AUTH0_CALLBACK_URL='http://192.168.1.103:3000/callback';
 
